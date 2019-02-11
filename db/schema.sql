99d401453ed2b4302561b4911a581f6efba59c45
RawKitMan <cjvaughn@rocketmail.com>
2019-02-11 09:12:33 -0700

    Set up initial folder structure, module installations, and server connections.

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+
+CREATE DATABASE moneymatch_db;
---
+++ models/player.js
@@ -0,0 +1,6 @@
+module.exports = function(sequelize, DataTypes) {
+    let Player = sequelize.define("Player", {
+      
+    });
+    return Player;
+  };
