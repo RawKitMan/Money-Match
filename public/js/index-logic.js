f2d5dcbfa7092e9607da74bb11a2375877e75cb8
Scripted Well <whigareda@theblackpilledpage.com>
2019-02-17 18:42:10 -0700

    1st draft of registration.js & initial file for index-logic.js

---
+++ public/js/index-logic.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    //NAVbar Buttons
+    document.getElementById("challenged").onclick(window.location = $('#li_challenged a').attr('href'));
+
+    document.getElementById("findChallenge").onclick(window.location = $('#li_findChallenge a').attr('href'));
+
+    document.getElementById("watchMatches").onclick(window.location = $('#li_watchMatches a').attr('href'));
+
+    document.getElementById("contactUs").onclick(window.location = $('#li_contactUs a').attr('href'));
+
+
+
+    //Decline Button with Modal
+})
--- public/js/registration.js
+++ public/js/registration.js
@@ -5,17 +5,18 @@ $(document).ready(function() {
     $('#signUp').click(function() {
         var name = $('.name').val();
         var email = $('.email').val();
+        var username = $('.username').val();
         var password = $('.password').val();
 
-        if(name == '' || email == '' || password == '') {
-            $('.name', '.email', '.password').next().show();
+        if(name == '' || email == '' || username == '' || password == '') {
+            $('.name', '.email', '.username', '.password').next().show();
             return false;
         }
         if(IsEmail(email)==false){
             $('invalid_email').show();
             return false;
         }
-        $.post("", $("#accountForm").serialize(), function(response) {
+        $.post("/api/players", $("#accountForm").serialize(), function(response) {
             $('#accountForm').fadeOut('slow', function() {
                 $('#correct').html(response);
                 $('#correct').fadeIn('slow');
@@ -39,7 +40,21 @@ function IsEmail(email) {
 //Sign-In
     $('.error').hide();
     $('#signIn').click(function() {
+        var login = $('.email').val();
+        var password = $('.password').val();
         //GET request for user credentials to compare
+        $.ajax({
+            url: '/api/players/' + username,
+            type: 'get',
+            dataType: 'json',
+            success: function(req, res) {
+                if (login == dbPlayer.username) {
+                    window.location = "/index"
+                } else {
+                    window.location = "/404"
+                }
+            }
+        })
         //if match go to the next page
         //else clear the form 
         //error message.
