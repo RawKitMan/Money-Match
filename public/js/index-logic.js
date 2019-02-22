b340090044ab9f63e06d011caaecd9de4862abc4
Scripted Well <whigareda@theblackpilledpage.com>
2019-02-21 20:11:08 -0700

    update to routes, buttons, etc.

--- public/js/index-logic.js
+++ public/js/index-logic.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
 
-    //NAVbar Buttons
-    document.getElementById("challenged").onclick(window.location = $('#li_challenged a').attr('href'));
-
-    document.getElementById("findChallenge").onclick(window.location = $('#li_findChallenge a').attr('href'));
-
-    document.getElementById("watchMatches").onclick(window.location = $('#li_watchMatches a').attr('href'));
-
-    document.getElementById("contactUs").onclick(window.location = $('#li_contactUs a').attr('href'));
-
-
-
     //Decline Button with Modal
+    $(".modal").modal();
+
+    //To DELETE from the challenges table
+    $("#decline").on("click", function () {
+        console.log("Lindsey is awesome!");
+        //AJAX call to delete from Challenges Table.
+    })
+
+    
 })
